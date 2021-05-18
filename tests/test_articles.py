4d91c28be82eedb1e55dd9671fc30eab317c1ed4
Carl Smith <carlsmith101@live.co.uk>
2021-05-18 19:50:23 +0100

    First commit

---
+++ tests/test_articles.py
@@ -0,0 +1,36 @@
+import requests
+
+BASE_URL = "https://5f99522350d84900163b8737.mockapi.io/tech-test/articles/"
+
+def test_valid_GET_articles_status_code_is_200():
+    response = requests.get(BASE_URL)
+    assert response.status_code == 200
+
+def test_PUT_article_status_code_is_400():
+    response = requests.put(url=BASE_URL)
+    assert response.status_code == 400 and response.text == '"Invalid request"'
+
+def test_POST_article_status_code_is_404():
+    response = requests.post(url=BASE_URL, data='')
+    assert response.status_code == 404 and response.text == '"Endpoint disabled"'
+
+def test_DELETE_article_status_code_is_400():
+    response = requests.delete(BASE_URL)
+    assert response.status_code == 400 and response.text == '"Invalid request"'
+
+
+def test_GET_article_status_code_is_200():
+    response = requests.get(BASE_URL + "1")
+    assert response.status_code == 200
+
+def test_PUT_article_status_code_is_400():
+    response = requests.put(url=BASE_URL + "1", data='')
+    assert response.status_code == 404 and response.text == '"Endpoint disabled"'
+
+def test_POST_article_status_code_is_400():
+    response = requests.post(url=BASE_URL + "1", data='')
+    assert response.status_code == 400 and response.text == '"Invalid request"'
+
+def test_DELETE_article_status_code_is_404():
+    response = requests.delete(BASE_URL + "1")
+    assert response.status_code == 404 and response.text == '"Endpoint disabled"'
